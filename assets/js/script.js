57c52412c1d567557d11c651be45f6ec9f408b71
Nxrix <nxrixz@gmail.com>
2023-11-20 21:42:57 +0330

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.body.insertAdjacentHTML("afterbegin",`<div id='menu_btn'>
     <div class="line"></div>
     <div class="line"></div>
   </div>
+  <button id="theme_btn"></button>
   <div id="menu">
     <h1 style="text-align:center">Menu</h1>
     <hr>
@@ -10,10 +11,142 @@ document.body.insertAdjacentHTML("afterbegin",`<div id='menu_btn'>
     <a class="linkBtn" href="../gallery.html">Gallery</a>
     <a class="linkBtn" href="../cube.html">Cube</a>
   </div>
-  <div id="scrollTrack"></div>
-  <div id="scrollBar"></div>`);
+  <div id="scroll_track"></div>
+  <div id="scroll_bar"></div>`);
 document.querySelector("#container").insertAdjacentHTML("afterbegin","<img class='banner' src='./img/png/banner.png' alt='banner'>");
-document.querySelector("#container").insertAdjacentHTML("beforeend","<br><hr style='width:100%;margin-top:0%;background:#dde;height:1px'><p style='margin-left:10%'><img style='width:32px;height:32px;margin-right:8px;' src='./img/svg/nxrix.svg'>Nxrix ©2023</p><br><div class='icon-list' style='margin-left:calc(10% - 10px)'><a href='https://github.com/nxrix'><svg class='icon' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 20 20'><path fill-rule='evenodd' d='M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z' clip-rule='evenodd'/></svg></a><a href='https://x.com/ZNxrix'><svg class='icon' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'><path fill='currentColor' d='M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z'/></svg></a><a href='https://discord.gg/5gukKTggNZ'><svg class='icon' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 21 16'><path d='M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z'/></svg></a></div>");
+document.querySelector("#container").insertAdjacentHTML("beforeend",`<br>
+<hr style="width:100%;margin-top:0%;height:1px">
+<p style="margin-left:10%">
+  <svg
+    style="width:32px;height:32px;margin-right:8px;"
+    width="64"
+    height="64"
+    viewBox="0 0 64 64"
+    fill="currentColor"
+    version="1.1"
+   id="svg239"
+   sodipodi:docname="deer.svg"
+   inkscape:export-filename="deer.svg"
+   inkscape:export-xdpi="96"
+   inkscape:export-ydpi="96"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs243">
+    <linearGradient
+       inkscape:collect="always"
+       id="linearGradient3797">
+      <stop
+         style="stop-color:#2b004c;stop-opacity:1;"
+         offset="0"
+         id="stop3793" />
+      <stop
+         style="stop-color:hsl(270,100%,25%);stop-opacity:1;"
+         offset="1"
+         id="stop3795" />
+    </linearGradient>
+    <linearGradient
+       id="linearGradient3789"
+       inkscape:swatch="solid">
+      <stop
+         style="stop-color:#000000;stop-opacity:1;"
+         offset="0"
+         id="stop3787" />
+    </linearGradient>
+    <linearGradient
+       inkscape:collect="always"
+       xlink:href="#linearGradient3797"
+       id="linearGradient3799"
+       x1="5"
+       y1="60"
+       x2="60"
+       y2="5"
+       gradientUnits="userSpaceOnUse" />
+  </defs>
+  <sodipodi:namedview
+     id="namedview241"
+     pagecolor="#505050"
+     bordercolor="#ffffff"
+     borderopacity="1"
+     inkscape:showpageshadow="0"
+     inkscape:pageopacity="0"
+     inkscape:pagecheckerboard="1"
+     inkscape:deskcolor="#505050"
+     showgrid="true">
+    <inkscape:grid
+       type="xygrid"
+       id="grid512"
+       empspacing="1"
+       originx="0"
+       originy="0" />
+  </sodipodi:namedview>
+  <path
+     fill-rule="evenodd"
+     clip-rule="evenodd"
+     d="m 20.516755,4.4812845 c -0.8049,0.530751 -0.9419,1.123121 -0.5855,2.533121 1.1239,4.4464995 2.7866,6.7676295 5.6682,7.9122995 1.5672,0.6223 2.2808,0.7188 5.9503,0.8043 l 3.4325,0.0799 -1.4638,1.1723 c -0.805,0.6448 -1.4636,1.2227 -1.4636,1.2843 0,0.0617 0.3739,0.875 0.8308,1.8075 0.7241,1.4777 0.8646,1.6883 1.0937,1.6388 0.1446,-0.0313 0.9273,-0.2613 1.739,-0.5111 1.8759,-0.5774 2.8166,-0.6153 3.1991,-0.1292 0.5465,0.6948 0.5095,0.9062 -0.8651,4.9592 -0.8281,2.4415 -1.4002,3.933 -1.6171,4.2157 -0.7044,0.9181 -0.2329,0.8746 -9.4791,0.8748 -9.448,2e-4 -8.4038,0.1208 -10.2763,-1.1868 -1.2476,-0.8712 -1.64699,-1.0631 -2.21249,-1.0625 -0.74012,8e-4 -1.31862,0.4394 -2.59425,1.9669 l -0.33812,0.4047 0.44325,0.7297 c 1.2185,2.0058 1.655,3.4656 1.3305,4.4488 -0.164,0.497 -2.48088,3.9873 -5.9413804,8.9505 -1.2915,1.8523 -1.520871,2.2547 -1.765746,3.0972 -0.25775,0.8866 -0.173375,1.9121 0.221375,2.6887 0.16275,0.3203 1.589251,2.3265 3.170121,4.4583 l 2.8742504,3.8761 3.10338,0.0017 3.10351,0.0019 -0.5953,-0.8437 c -2.364,-3.351 -4.16184,-6.1807 -4.42021,-6.9572 -0.559,-1.6796 -0.33238,-2.6812 1.04912,-4.6366 0.38863,-0.55 1.16009,-1.6622 1.71449,-2.4715 0.6536,-0.9541 1.195924,-1.473966 1.5265,-1.8241 0.365775,-0.353675 0.75622,-0.713323 1.183903,-0.239339 0.330272,0.366027 0.194834,0.627839 -0.785103,1.856939 -2.4048,3.5321 -2.72141,4.0226 -3.02629,4.6888 -0.286,0.6247 -0.31012,0.7915 -0.233,1.614 0.0975,1.0415 0.36113,1.5297 2.42909,4.5002 0.6222,0.8938 1.5687,2.2854 2.103,3.0926 l 0.9719,1.4675 3.292,0.0013 3.292,0.0011 -0.4349,-0.7812 c -0.3966,-0.7127 -3.5145,-6.9147 -4.013,-7.9829 l -0.2107,-0.4515 1.2377,-2.0485 c 2.2454,-3.7156 2.4608,-4.0304 3.0814,-4.5028 1.1728,-0.8928 2.282,-1.0825 6.6305,-1.1337 l 3.125,-0.0369 0.694,0.3409 c 1.0524,0.5167 1.8793,1.7535 4.62,6.9091 0.3654,0.6875 0.76,1.4209 0.8769,1.6296 0.1169,0.2088 0.8887,1.615 1.7152,3.125 0.8267,1.51 1.7772,3.2235 2.1123,3.8079 l 0.6092,1.0625 3.1662,0.0334 c 1.7412,0.0184 3.1456,-0.0098 3.1207,-0.0625 -0.0249,-0.0528 -0.2461,-0.4896 -0.4916,-0.9709 -1.8121,-3.5521 -7.6568,-15.3037 -7.9818,-16.0487 -0.3496,-0.8015 -0.3922,-1.0107 -0.3211,-1.5798 0.0451,-0.3609 0.1596,-0.7607 0.2546,-0.8887 0.1669,-0.225 0.1922,-0.2225 0.7592,0.0746 0.7092,0.3715 4.9903,3.1762 5.0862,3.3321 0.0374,0.0608 -0.3249,1.0105 -0.805,2.1105 -0.4801,1.1 -0.8994,2.1125 -0.9317,2.25 -0.047,0.1996 0.1863,0.4264 1.1581,1.125 0.6692,0.4813 1.6569,1.1984 2.1947,1.5938 0.5378,0.3953 1.0054,0.7187 1.0392,0.7187 0.0828,0 2.9873,-5.8106 3.5301,-7.0625 0.8284,-1.9106 0.5677,-3.29 -0.8238,-4.3599 -0.3886,-0.2987 -2.3941,-1.6001 -4.4566,-2.892 -5.981,-3.746 -6.1711,-3.8926 -6.2779,-4.8398 -0.0768,-0.6824 0.2642,-1.9637 1.5063,-5.6583 1.1627,-3.4586 1.2184,-3.5866 1.7271,-3.9741 0.6981,-0.5318 1.4228,-0.6478 5.1695,-0.8268 1.3063,-0.0625 2.5719,-0.1757 2.8125,-0.2517 0.2406,-0.076 0.704,-0.3344 1.0298,-0.5741 0.9625,-0.7085 1.4777,-1.7178 1.4607,-2.8607 -0.0197,-1.32 -0.7232,-2.4047 -1.9241,-2.9666 -0.8603,-0.4026 -1.7035,-0.5629 -6.4414,-1.2242 -4.7882,-0.66842 -6.5117,-0.89517 -9.4375,-1.2418 l -2.875,-0.34075 -0.0844,-0.49462 c -0.0465,-0.272 -0.1432,-1.029 -0.2151,-1.68213 -0.286,-2.6019995 -1.2212,-4.2148695 -2.953,-5.0928695 -1.6169,-0.819751 -2.81,-0.222126 -2.81,1.40762 0,0.84963 0.2676,1.25238 1.1168,1.68113 0.4046,0.20425 0.8478,0.52887 0.985,0.7215 0.323,0.45362 0.612,1.3552495 0.8185,2.5537495 l 0.1651,0.9585 -1.1989,0.09025 c -1.5887,0.1195 -4.2075,-0.0089 -5.446,-0.267 -2.5537,-0.53238 -3.5875,-1.65525 -4.6719,-5.0746295 -0.2762,-0.87125 -0.5931,-1.75975 -0.704,-1.97425 -0.4176,-0.807621 -1.6108,-1.135246 -2.3527,-0.646121 z m 26.5827,12.2661205 c -0.3973,0.1603 -0.6696,0.6226 -0.6696,1.137 0,0.9224 0.9406,1.5489 1.7918,1.1933 0.241,-0.1008 0.4988,-0.2967 0.5732,-0.4355 0.255379,-0.393797 0.313337,-1.114031 -0.0252,-1.5297 -0.3465,-0.4512 -1.0667,-0.6087 -1.6702,-0.3651 z"
+     id="path237"
+     sodipodi:nodetypes="ccccccscccccssccccccccccccccccscscccccccccsccccccccccccccccccsscccssccccccsccccccccssccccccccccscccc"/>
+     <!--url(#linearGradient3799)-->
+  </svg>
+  Nxrix ©2023
+</p><br>
+<div class="icon-list" style="margin-left:calc(10% - 10px);font-size:0;">
+  <a href="https://github.com/nxrix">
+    <svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/></svg>
+  </a>
+  <a href="https://t.me/Nxrix">
+    <svg class="icon" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M41.4193 7.30899C41.4193 7.30899 45.3046 5.79399 44.9808 9.47328C44.8729 10.9883 43.9016 16.2908 43.1461 22.0262L40.5559 39.0159C40.5559 39.0159 40.3401 41.5048 38.3974 41.9377C36.4547 42.3705 33.5408 40.4227 33.0011 39.9898C32.5694 39.6652 24.9068 34.7955 22.2086 32.4148C21.4531 31.7655 20.5897 30.4669 22.3165 28.9519L33.6487 18.1305C34.9438 16.8319 36.2389 13.8019 30.8426 17.4812L15.7331 27.7616C15.7331 27.7616 14.0063 28.8437 10.7686 27.8698L3.75342 25.7055C3.75342 25.7055 1.16321 24.0823 5.58815 22.459C16.3807 17.3729 29.6555 12.1786 41.4193 7.30899Z" fill="currentColor"/>
+    </svg>
+  </a>
+  <a href="https://x.com/ZNxrix">
+    <svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M12.186 8.672 18.743.947h-2.927l-5.005 5.9-4.44-5.9H0l7.434 9.876-6.986 8.23h2.927l5.434-6.4 4.82 6.4H20L12.186 8.672Zm-2.267 2.671L8.544 9.515 3.2 2.42h2.2l4.312 5.719 1.375 1.828 5.731 7.613h-2.2l-4.699-6.237Z"/></svg>
+  </a>
+  <a href="https://discord.gg/5gukKTggNZ">
+    <svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16"><path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/></svg>
+  </a>
+</div>`);
+
+var menu = document.getElementById("menu");
+var menu_btn = document.getElementById("menu_btn");
+var scroll_bar = document.getElementById("scroll_bar");
+var theme_btn = document.getElementById("theme_btn");
+
+var svg_light_mode = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>`;
+var svg_dark_mode = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="currentColor"><path d="M480-360q50 0 85-35t35-85q0-50-35-85t-85-35q-50 0-85 35t-35 85q0 50 35 85t85 35Zm0 80q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Zm326-268Z"/></svg>`;
+
+theme_meta = document.createElement("meta");
+document.head.appendChild(theme_meta);
+theme_meta.setAttribute("name","theme-color");
+
+if (localStorage.getItem("theme")==null)  {
+  document.documentElement.setAttribute("data-theme","light");
+  theme_meta.setAttribute("content","#eef");
+  theme_btn.innerHTML = svg_light_mode;
+}
+else {
+  document.documentElement.setAttribute("data-theme",localStorage.getItem("theme"));
+  theme_meta.setAttribute("content",localStorage.getItem("theme")=="light"?"#eef":"#000");
+  theme_btn.innerHTML = localStorage.getItem("theme")=="light"?svg_light_mode:svg_dark_mode;
+}
+
+theme_btn.onclick = () => {
+  if (document.documentElement.getAttribute("data-theme")=="dark") {
+    theme_btn.innerHTML = svg_light_mode;
+    document.documentElement.setAttribute("data-theme","light");
+    theme_meta.setAttribute("content","#eef");
+    localStorage.setItem("theme","light");
+  }
+  else {
+    theme_btn.innerHTML = svg_dark_mode;
+    document.documentElement.setAttribute("data-theme","dark");
+    theme_meta.setAttribute("content","#000");
+    localStorage.setItem("theme","dark");
+  }
+}
+
 menu_btn.onclick = () => {
   if (menu.classList.contains("active")==true) {
     menu_btn.classList.remove("active");
@@ -25,7 +158,7 @@ menu_btn.onclick = () => {
   }
 }
 scroll = () => {
-  scrollBar.style.top = container.scrollTop/((container.scrollHeight - window.innerHeight)||1)*(25-4)+37.5+"%";
+  scroll_bar.style.top = container.scrollTop/((container.scrollHeight - window.innerHeight)||1)*(25-4)+37.5+"%";
 }
 container.addEventListener("scroll",scroll);
 scroll();
