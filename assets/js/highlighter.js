fe635baa48a1efef4f5a19872a12d11c81cd84e4
Nxrix <nxrixz@gmail.com>
2025-01-31 13:07:00 +0330

    Update highlighter.js

--- assets/js/highlighter.js
+++ assets/js/highlighter.js
@@ -1,6 +1,6 @@
-  //-----------------------------------//
- //--- Nxrix - All rights reserved ---//
-//-----------------------------------//
+/**
+ * @copyright   (c) 2025 Nxrix. All rights reserved.
+ */
 
 const highlighter = {};
 
