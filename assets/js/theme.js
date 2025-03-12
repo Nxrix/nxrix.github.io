b02044bffb53547dbe4c4fd4583669e41ba2d7f4
Nxrix <nxrixz@gmail.com>
2025-03-12 21:31:23 +0330

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,4 +1,5 @@
 let theme = localStorage.getItem("theme")||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light");
+document.documentElement.setAttribute("data-theme",theme);
 const change_theme = () => {
   theme = (theme=="dark"?"light":"dark");
   localStorage.setItem("theme",theme);
