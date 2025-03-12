d46970459f5a155c39ace2d8c056ed26ddac4c17
Nxrix <nxrixz@gmail.com>
2025-03-12 21:41:45 +0330

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,7 +1,9 @@
 let theme = localStorage.getItem("theme")||(window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light");
 document.documentElement.setAttribute("data-theme",theme);
+document.querySelector('meta[name="theme-color"]').setAttribute("content",(theme?"#fff":"#000"));
 const change_theme = () => {
   theme = (theme=="dark"?"light":"dark");
   localStorage.setItem("theme",theme);
   document.documentElement.setAttribute("data-theme",theme);
+  document.querySelector('meta[name="theme-color"]').setAttribute("content",(theme?"#fff":"#000"));
 }
