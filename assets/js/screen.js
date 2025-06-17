7f9adc46d036d672b55f92596390360a9e7ea225
Nxrix <nxrixz@gmail.com>
2025-06-17 12:53:21 +0330

    Update screen.js

--- assets/js/screen.js
+++ assets/js/screen.js
@@ -1,7 +1,7 @@
 window.resize = () => {
-  var root = document.querySelector(":root");
-  var width = window.innerWidth;
-  var height = window.innerHeight;
+  const root = document.querySelector(":root");
+  const width = window.innerWidth;
+  const height = window.innerHeight;
   root.style.setProperty("--vw",width+"px");
   root.style.setProperty("--vh",height+"px");
   root.style.setProperty("--vmin",(width<height?width:height)+"px");
